60d9506a2d8af1aef8ecc27d88b3c10851166ad0
Junior Mora <100890725+juniormoram@users.noreply.github.com>
2025-02-21 13:58:50 -0600

    CodeFormat

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
+function createSquare() {
+  const section = document.querySelector("section");
+  const square = document.createElement("span");
+  var size = Math.random() * -10;
 
-function createSquare(){
-    const section = document.querySelector('section');
-    const square = document.createElement('span');
-    var size = Math.random() * -10;
-    
-    square.style.width = 3 + size + 'px';
-    square.style.height = 7 + size + 'px';
-    
-    square.style.top = Math.random() * innerHeight + 'px';
-    square.style.left = Math.random() * innerWidth + 'px';
-    
-    section.appendChild(square);
-  
-  }
-  setInterval(createSquare , 15)
+  square.style.width = 3 + size + "px";
+  square.style.height = 7 + size + "px";
+
+  square.style.top = Math.random() * innerHeight + "px";
+  square.style.left = Math.random() * innerWidth + "px";
+
+  section.appendChild(square);
+}
+setInterval(createSquare, 15);
