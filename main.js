357f984b8bf095f2d0ec84e5651937dd94c21290
Junior Mora <100890725+juniormoram@users.noreply.github.com>
2025-02-27 17:59:02 -0600

    AjustesVarios

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function createSquare() {
   const square = document.createElement("span");
   var size = Math.random() * -10;
 
-  square.style.width = 3 + size + "px";
+  square.style.width = 2 + size + "px";
   square.style.height = 7 + size + "px";
 
   square.style.top = Math.random() * innerHeight + "px";
