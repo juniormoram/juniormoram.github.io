20923f1dacab641014413e1b0b8977c79e7e6020
juniormoram <100890725+juniormoram@users.noreply.github.com>
2026-01-31 19:44:23 -0600

    Configuración para Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/',
   server: {
     host: "::",
     port: 8080,
     hmr: {
       overlay: false,
     },
-  },
+  }, 
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
